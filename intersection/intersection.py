00863c49c9e08d6292e4d86f084bd12c76da5281
Chris Park <chrispark455@gmail.com>
2017-04-07 12:41:34 -0400

    finished writing intersection
    - right now it shows absolute counts (number of shared Ag contact
    residues)

--- intersection/intersection.py
+++ intersection/intersection.py
@@ -9,13 +9,13 @@ import os
 #
 # ARGUMENTS
 # 1. files = array: array of file names with the results
-# 2. out = output file name where results will be written
-#	3. threshold = how many contacts determines a "contact" across 
+#	2. threshold = how many contacts / percent determines a "contact" across 
+# 3. out = output file name where results will be written
 #
 # RETURNS 
-# 	1. array of tuples, tuple = (model comparison pair, percentage intersection)
+# 	1. array of tuples, tuple = (str - model comparison pair, int - percentage intersection)
 
-def intersection(files, out, threshold):
+def intersection(files, threshold, out):
 
 	# stores all files
 	data = {}
@@ -33,6 +33,7 @@ def intersection(files, out, threshold):
 	# nested loop to go through all pairs of files
 	for i in range(len(files)):
 		for j in range(i+1, len(files)):
+			print i, j
 			file_one = files[i]
 			file_two = files[j]
 
@@ -49,28 +50,28 @@ def intersection(files, out, threshold):
 				# **** RESIDUE NUMBERS MUST BE IN EXACT SAME ORDER ACROSS ALL FILES
 				res_one = line_one[5:8]
 				res_two = line_two[5:8]
-				total_one = int(line_one[10:].strip())
-				total_two = int(line_two[10:].strip())
+				total_one = float(line_one[10:].strip())
+				total_two = float(line_two[10:].strip())
 
-				common = abs(total_one - total_two)
-				intersection += common
+				if (total_one >= threshold and total_two >= threshold):
+					intersection += 1
 
 			# write to return array
-			result = (file_one + " & " + file_two, intersection)
+			result = (file_one[7:13] + " & " + file_two[7:13], intersection)
 			results.append(result)
 
-		output = open(out, 'w')
-		for result in results:
-			output.write(result[0][7:13]+": "+str(result[1])+"\n")
+	output = open(out, 'w')
+	for result in results:
+		output.write(result[0]+": "+str(result[1])+"\n")
 
-		output.close()
+	output.close()
 
-		print results
-		return results
+	print results
+	return results
 
 intersection(["totals_D206m2.txt", "totals_D206m9.txt", "totals_D206mR.txt",
 	"totals_D410m1.txt", "totals_D410m5.txt", "totals_D410mR.txt",
-	"totals_D430m7.txt", "totals_D430m8.txt", "totals_D430mR.txt"], "output.txt")
+	"totals_D430m7.txt", "totals_D430m8.txt", "totals_D430mR.txt"], 0.15, "output.txt", )
 
 
 
