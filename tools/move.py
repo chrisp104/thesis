61771d55b7670d8d184917f1f514ff8592f4a97f
Chris Park <chrispark455@gmail.com>
2017-05-03 01:16:27 -0400

    made contact files

---
+++ tools/move.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+for antibody in os.listdir("/Users/Chris/GitHub/thesis/antibodies/"):
+	if antibody[0] != 'D': continue
+	os.chdir("/Users/Chris/GitHub/thesis/antibodies/"+antibody+"/decoys")
+	scores = open('scores.txt', 'r')
+	lines = scores.readlines()
+	scores.close()
+	if len(lines) == 0:
+		continue
+	first = lines[0]
+	best = first[11:12]
+	model = 'm'+best
+	os.makedirs("/Users/Chris/GitHub/thesis/mutagenesis/one_models/"+antibody+"/")
+	# os.makedirs("/Users/Chris/GitHub/thesis/mutagenesis/one_models/"+antibody+"/"+model+"/")
+	shutil.copytree("/Users/Chris/GitHub/thesis/contacts/all_indiv/"+antibody+"/"+model+"/", 
+		"/Users/Chris/GitHub/thesis/mutagenesis/one_models/"+antibody+"/"+model+"/")
