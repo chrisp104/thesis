86a0a9391a501022025893dd0e51adabfa9f4fa2
Chris Park <chrispark455@gmail.com>
2017-05-29 16:15:33 -0400

    Condensed FINAL

--- mutagenesis/tools/test.py
+++
@@ -1,8 +0,0 @@
-from cluster_variants import *
-
-
-final_clusters = clusterBestVariants("/Users/Chris/GitHub/thesis/mutagenesis/variants.txt", 
-	"/Users/Chris/GitHub/thesis/mutagenesis/merged_isdb.pdb", 3, 
-	"/Users/Chris/GitHub/thesis/mutagenesis/test_out.txt")
-
-findBestFinalVariants(final_clusters, "/Users/Chris/GitHub/thesis/mutagenesis/all.txt")
